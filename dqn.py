7d9ec06e0f41be68e81528fda54c08c1517e664c
Jacob Campbell <ja.merl.campbell@gmail.com>
2018-02-23 01:12:20 -0600

    Trying to get more of a handle on how TF works at a low level. Need to work out how to have multiple models at once, and how those models are represented in the TF ether. I may be overthinking this, but coming from a more Keras focused background this has taken some getting used to.

--- dqn.py
+++ dqn.py
@@ -5,19 +5,26 @@ from __future__ import print_function
 import numpy as np
 import tensorflow as tf
 
-import logging
+export_dir = ('/home/merlin/rl/models')
 
+    #def __init__(self, behavior_policy='e-greedy', epsilon = 0.05, gamma=0.99,
+    #             m=4, batch_size=32, k=4, valid_actions=5, recall_frames=1000,
+    #             clip=True, C=50):
 class DQN(object):
-    def __init__(self, behavior_policy='e-greedy', epsilon = 0.05, gamma=0.99, m=4, batch_size=32, k=4, valid_actions=5, recall_frames=1000, clip=True):
-        self.behavior_policy = behavior_policy
+    def __init__(self, lr=.00025, gamma=0.99, m=4, batch_size=32, valid_actions=6, clip=True):
+        self.learning_rate = .00025
         self.gamma = gamma
-        self.k = k
+        self.m = m
         self.batch_size = batch_size
-        self.recall_frames = recall_frames
+        self.valid_actions = valid_actions
         self.clip = clip
+        self.graph = tf.Graph()
 
-    def build_ddn(self):
-        input_layer = tf.placeholder([batch_size, 84, 84, m], tf.int32)
+        self.builder = tf.saved_model_builder.SavedModelBuilder(export_dir)
+
+    def build_dqn(self):
+        input_layer = tf.placeholder([batch_size, 84, 84, m], tf.float32)
+
         # First convolutional layer -- Input layer
         conv1 = tf.layers.conv2d(inputs=input_layer,
                                  filters=32,
@@ -52,11 +59,18 @@ class DQN(object):
                                  activation=tf.nn.relu)
 
         # Output layer
-        output_layer = tf.layers.dense(inputs=dense1,
-                                       units=self.valid_actions)
+        out = tf.layers.dense(inputs=dense1,
+                              units=self.valid_actions)
 
-        values = output_layer
+        self.out = out
+        self.predict = tf.argmax(out, 1)
+        self.Y = tf.placeholder(tf.float32, [None, valid_actions])
+        self.loss = tf.mean_squared_error(Y, self.predict)
+        self.optimizer = tf.train.RMSPropOptimizer(
+                learning_rate=self.learning_rate).minimize(self.loss)
 
+        with tf.Session(graph=self.graph) as sess:
+            sess.run(tf.global_variables_initializer())
+            self.builder.add_meta_graph_and_variables(sess, ["estimate"])
 
-        return self.model
-
+        self.builder.save()
