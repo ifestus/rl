d251e5c1d72f1b203d2d5e57b206c62103a9a5c3
Jacob Campbell <ja.merl.campbell@gmail.com>
2018-02-23 20:13:47 -0600

    after some more research, it turns out i don't have to have any of that namespace/graph stuff set up myself. I seem to have been overthinking things, but I think we can finally move forward from ehre

--- dqn.py
+++ dqn.py
@@ -11,7 +11,7 @@ export_dir = ('/home/merlin/rl/models')
     #             m=4, batch_size=32, k=4, valid_actions=5, recall_frames=1000,
     #             clip=True, C=50):
 class DQN(object):
-    def __init__(self, lr=.00025, gamma=0.99, m=4, batch_size=32, valid_actions=6, clip=True, name):
+    def __init__(self, lr=.00025, gamma=0.99, m=4, batch_size=32, valid_actions=6, clip=True, name=None):
         self.learning_rate = .00025
         self.gamma = gamma
         self.m = m
@@ -19,58 +19,68 @@ class DQN(object):
         self.valid_actions = valid_actions
         self.clip = clip
         self.name = name
-
+        self.session = tf.Session()
         self.builder = tf.saved_model_builder.SavedModelBuilder(export_dir)
 
     def build_dqn(self):
-        with tf.name_scope(name):
-            input_layer = tf.placeholder([batch_size, 84, 84, m], tf.float32)
+        input_layer = tf.placeholder([batch_size, 84, 84, m], tf.float32)
 
-            # First convolutional layer -- Input layer
-            conv1 = tf.layers.conv2d(inputs=input_layer,
-                                     filters=32,
-                                     strides=[1, 4, 4, 1],
-                                     kernel_size=[8, 8],
-                                     padding="valid", # try same, too
-                                     activation=tf.nn.relu,
-                                     name="input_conv")
+        # First convolutional layer -- Input layer
+        conv1 = tf.layers.conv2d(inputs=input_layer,
+                                 filters=32,
+                                 strides=[1, 4, 4, 1],
+                                 kernel_size=[8, 8],
+                                 padding="valid", # try same, too
+                                 activation=tf.nn.relu,
+                                 name="input_conv")
 
-            # Second convolutional layer. Input is the output of the previous layer.
-            conv2 = tf.layers.conv2d(inputs=conv1,
-                                     filters=64,
-                                     strides=[1, 2, 2, 1],
-                                     kernel_size=[4, 4],
-                                     padding="valid", # try same, too
-                                     activation=tf.nn.relu,
-                                     name="conv2")
+        # Second convolutional layer. Input is the output of the previous layer.
+        conv2 = tf.layers.conv2d(inputs=conv1,
+                                 filters=64,
+                                 strides=[1, 2, 2, 1],
+                                 kernel_size=[4, 4],
+                                 padding="valid", # try same, too
+                                 activation=tf.nn.relu,
+                                 name="conv2")
 
-            # Third convolutional layer
-            conv3 = tf.layers.conv2d(inputs=conv2,
-                                     filters=64,
-                                     strides=np.ones([4]),
-                                     kernel_size=[3, 3],
-                                     padding="valid", # try same, too
-                                     activation=tf.nn.relu,
-                                     name="conv3")
+        # Third convolutional layer
+        conv3 = tf.layers.conv2d(inputs=conv2,
+                                 filters=64,
+                                 strides=np.ones([4]),
+                                 kernel_size=[3, 3],
+                                 padding="valid", # try same, too
+                                 activation=tf.nn.relu,
+                                 name="conv3")
 
-            # Dense hidden layer with 512 rectifier units
-            conv3_flat = tf.reshape(conv3, [-1, 7 * 7 * 64])
-            dense1 = tf.layers.dense(inputs=conv3_flat,
-                                     units=512,
-                                     activation=tf.nn.relu)
+        # Dense hidden layer with 512 rectifier units
+        conv3_flat = tf.reshape(conv3, [batch_size, 7 * 7 * 64])
+        dense1 = tf.layers.dense(inputs=conv3_flat,
+                                 units=512,
+                                 activation=tf.nn.relu)
 
-            # Output layer
-            out = tf.layers.dense(inputs=dense1,
-                                  units=self.valid_actions)
-            self.out = out
-            self.predict = tf.argmax(out, 1)
-            self.Y = tf.placeholder(tf.float32, [None, valid_actions])
-            self.loss = tf.mean_squared_error(Y, self.predict)
-            self.optimizer = tf.train.RMSPropOptimizer(
-                    learning_rate=self.learning_rate).minimize(self.loss)
+        # Output layer
+        out = tf.layers.dense(inputs=dense1,
+                              units=self.valid_actions)
+        self.out = out
+        self.predict = tf.argmax(out, 1)
+        self.Y = tf.placeholder(tf.float32, [None, valid_actions])
+        self.loss = tf.mean_squared_error(Y, self.predict)
+        self.optimizer = tf.train.RMSPropOptimizer(
+                learning_rate=self.learning_rate).minimize(self.loss)
 
-        # with tf.Session(graph=self.graph) as sess:
-        #     sess.run(tf.global_variables_initializer())
-        #     self.builder.add_meta_graph_and_variables(sess, ["estimate"])
+    def init_globals(self):
+        self.session.run(tf.global_variables_initializer())
 
+    def fit(self):
+        pass
+
+    def predict(self):
+        pass
+
+    def close(self):
+        tf.reset_default_graph()
+        self.session.close()
+
+    def save(self):
+        self.builder.add_meta_graph_and_variables(self.session, [self.name])
         self.builder.save()
