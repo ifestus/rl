c4306475b6c9502a4f6ae91badee3dd0973195ff
Jacob Campbell <ja.merl.campbell@gmail.com>
2018-02-24 20:32:47 -0600

    .

--- dqn.py
+++ dqn.py
@@ -1,3 +1,4 @@
+# architecture idea from https://github.com/DongjunLee/dqn-tensorflow/
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
