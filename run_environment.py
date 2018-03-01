1ba47f59c4aab896f5b07fa438e933c18cee164e
Jacob Campbell <ja.merl.campbell@gmail.com>
2018-02-28 23:53:58 -0600

    working on implementing experience replay

--- run_environment.py
+++ run_environment.py
@@ -11,40 +11,67 @@ print(env.action_space)
 
 sess = tf.Session()
 
-D = [None] * 1000000
+exp_max_frames = 100000
+D = []
 X_size = (84, 84, 1)
+# Number of frames our sample will take
+m = 4
 
-DQN_behavior = DQN(sess, name="behavior")
+DQN_estimate = DQN(sess, name="estimate")
 DQN_target   = DQN(sess, name="target")
 
 
+# Prefill D
+
+def sample_experience(minibatch_size=32):
+    # Fill the sample with zeros to begin with
+    sample = np.zeros((minibatch, X_size[0], X_size[1], X_size[2]))
+    # We want to add ${minibatch} experience values to our samples
+    for j in range(minibatch):
+        # k is the index to our experience pool (D)
+        k = np.randint(exp_max_frames)
+        sample[j] = D[k]
+
+
 for i_episode in range(20):
-    observation = env.reset()
+    obs = env.reset()
     reward = 0
+    action = 0
     for t in range(1000):
+        env.render()
+        # Get action from the estimate network
+        # this action is going to be an e-greedy one that is determined by the
+        # estimate network
+        obs = obs_pre
+
+        DQN_estimate.predict(np.expand_dims(obs, 0))
+        action = env.action_space.sample()
+
+        # take action and preprocess next state
+        next_obs, reward, done, info = env.step(action)
+
         # Parse observation and create input for models
-        obs = transform.resize(observation, X_size)
+        obs_pre = transform.resize(next_obs, X_size)
 
         # Clip reward to +-0
         r = np.clip(reward, -1, 1)
 
-        env.render()
-        # Get action from the behavior network
-        # this action is going to be an e-greedy one that is determined by the
-        # behavior network
-        obs = np.concatenate([obs, obs, obs, obs], -1)
+        # Update our experience pool
+        # We need to have some kind of cache here so we can store the m most
+        # recent experience tuples together
+        experience = (obs, action, r, obs_pre)
+        _ = D.pop()
+        D.append(experience)
 
-        DQN_behavior.predict(np.expand_dims(obs, 0))
-        action = env.action_space.sample()
-
-        # take action
-        observation, reward, done, info = env.step(action)
+        # Sample minibatch of transitions
+        # Gradient descent
+        # Reset Q_target = Q_estimate
 
         # Marks the end of an episode
         if done:
             print("Episode finished after {} timesteps.".format(t+1))
             break
 
-DQN_behavior.close()
+DQN_estimate.close()
 
 
