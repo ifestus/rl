2f39194571f252e514b4b7fec9719c4e9749284c
Jacob Campbell <ja.merl.campbell@gmail.com>
2018-03-15 22:33:35 -0500

    initial commit for implementing DDQN variant

--- dqn.py
+++ dqn.py
@@ -8,6 +8,18 @@ from dqn_models import CNN
 import numpy as np
 import tensorflow as tf
 
+class DDQN(DQN):
+    def __init__(self, session, name="CNN", lr=.00025, gamma=0.99, m=4, batch_size=32, valid_actions=6, clip=True):
+        super().__init__(session, name, lr, gamma, m, batch_size, valid_actions, clip)
+
+    def predict(self, X):
+        return self.session.run(self.model.predict,
+                                feed_dict={self._X: X})
+
+    def update(self, X, Y):
+        self.session.run([self._optimizer, self._loss],
+                         feed_dict={self._X: X, self._Y: Y})
+
 class DQN(object):
     def __init__(self, session, name="CNN", lr=.00025, gamma=0.99, m=4, batch_size=32, valid_actions=6, clip=True):
         self._X = tf.placeholder(tf.float32, [None, 84, 84, m])
